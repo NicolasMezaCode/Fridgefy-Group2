72dbf63512387aabac3a3f3cf20a2469d275b7ef
Nicolas Meza <nickismeza@gmail.com>
2022-08-30 13:28:41 -0700

    Api keys added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BackendTest } from './Backend/FetchAPI'
+import Main from './Frontend/Components/Main';
 
 function App() {
   return (
     <div className="App">
-      <BackendTest />
+      <Main/>
     </div>
   );
 }
