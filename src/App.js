f8820c4cf013bf50d9303a45825bdc306836be6d
Yuto Yamakita <yuvto.0420@gmail.com>
2022-08-30 16:58:00 -0700

    Setting up auth with email and password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-import { BackendTest, FetchRecipes } from './Backend/FetchRecipes'
+import AuthProvider from './AuthContext';
+// import { FetchRecipes } from './Backend/FetchRecipes'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import SignUp from './Frontend/User/SignUp';
+import SignIn from './Frontend/User/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <FetchRecipes />
+      {/* <FetchRecipes /> */}
+      <AuthProvider>
+        <BrowserRouter>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/signup'>Sign up</Link>
+          <Link to='/signin'>Sign in</Link>
+          <Link to='/recipes'>My recipes</Link>
+          <Link to='/shoppinglist'>My Shopping List</Link>
+        </nav>
+          <Routes>
+            <Route path='/signup' element={<SignUp />}></Route>
+            <Route path='/signin' element={<SignIn />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Frontend/User/SignUp.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react'
+import { auth } from '../../Backend/firebase_config';
+import { createUserWithEmailAndPassword, signInWithPopup,
+  GoogleAuthProvider, } from 'firebase/auth';
+import { useAuthContext } from '../../AuthContext';
+
+const SignUp = () => {
+
+    const { user } = useAuthContext()
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef()
+
+    const SignUpHandleSubmit = (e) =>{
+        e.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+        console.log('submit', enteredEmail, enteredPassword)
+
+        createUserWithEmailAndPassword(auth, enteredEmail, enteredPassword);
+    }
+  return (
+    <>
+    <h2>Sign up here</h2>
+    <form onSubmit={SignUpHandleSubmit}>
+        <div>
+          <label htmlFor='emailInput'>Email</label>
+          <input type='email' ref={emailInputRef} />
+        </div>
+        <label htmlFor='passwordInput'>Password</label>
+        <input type='password' ref={passwordInputRef} />
+        <div>
+          <button>Sign up</button>
+        </div>
+    </form>
+      <button onClick={() => console.log('logged in with Google')}>
+        Sign in with Google
+      </button>
+    </>
+  )
+}
+
+export default SignUp
