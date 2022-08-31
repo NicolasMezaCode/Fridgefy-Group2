53bc5c2998e8511b9298e3d7aa8801cf9ff1a5e3
Nicolas Meza <nickismeza@gmail.com>
2022-08-31 15:07:35 -0700

    adding filter

--- src/Frontend/Components/Searchbar.jsx
+++ src/Frontend/Components/Searchbar.jsx
@@ -8,22 +8,34 @@ import axios from 'axios';
 export default function Searchbar() {
     const searchInput=useRef();
     const [option,setOption]=useState("5");
+    const [cuisine,setCuisine]=useState("");
+    const[diet,setDiet]=useState("");
+    const[intolerance,setIntolerance]=useState("")
     const {recipes,addRecipe}=useContext(RecipeContext)
 
-    useEffect(()=>{async function fetchApi(){
-        const response=await axios.get(`${process.env.REACT_APP_SPOONACULAR_RANDOM_URL}?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=10&tags=dessert`);
-        console.log(response.data.recipes)
-        addRecipe(response.data.recipes)
+    // useEffect(()=>{async function fetchApi(){
+    //     const response=await axios.get(`${process.env.REACT_APP_SPOONACULAR_RANDOM_URL}?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=10&tags=dessert`);
+    //     console.log(response.data.recipes)
+    //     addRecipe(response.data.recipes)
+    // }
+    // fetchApi()
+    // },[]);
+    const handleCuisine=(e)=>{
+      setCuisine(e.target.value)
     }
-    fetchApi()
-    },[])
+    const handleDiet=(e)=>{
+      setDiet(e.target.value)
+    }
+    const handleIntolerance=(e)=>{
+      setIntolerance(e.target.value)
+    }
     const handleNumber=(e)=>{
         setOption(Number(e.target.value))
     }
     const searchHandler=(e)=>{
         const search=searchInput.current.value
         e.preventDefault()
-        addRecipe(getRecipeDataViaKeyword(search,option))
+        addRecipe(getRecipeDataViaKeyword(search,cuisine,diet,intolerance,option))
     }
   return (
     <>
@@ -36,6 +48,29 @@ export default function Searchbar() {
         <option value="20">20</option>
         <option value="25">25</option>
         <option value="30">30</option>
+      </select>
+      <select onChange={handleDiet} name="filterDiet" id="">
+        <option value="">None</option>
+        <option value="vegan">Vegan</option>
+        <option value="vegetarian">Vegetarian</option>
+        <option value="paleo">Paleo</option>
+        <option value="glutenfree">GlutenFree</option>
+      </select>
+      <select onChange={handleCuisine} name="filterCuisine" id="">
+        <option value="">None</option>
+        <option value="american">American</option>
+        <option value="italian">Italian</option>
+        <option value="french">French</option>
+        <option value="korean">Korean</option>
+        <option value="chinese">Chinese</option>
+      </select>
+      <select onChange={handleIntolerance} name="filterIntolerance" id="">
+        <option value="">None</option>
+        <option value="egg">Egg</option>
+        <option value="peanut">Peanut</option>
+        <option value="gluten">Gluten</option>
+        <option value="grain">Grain</option>
+        <option value="sesame">Sesame</option>
       </select>
       <button>Search</button>
     </form>
